56bb10760fc127377836dc4606f2d6cd1902aada
Shaun Yee <yeezy@Shauns-MacBook-Pro.local>
2018-06-27 16:28:19 -0700

    hits api for movie database and sets state to movie list

--- src/App.js
+++ src/App.js
@@ -3,29 +3,35 @@ import Movie from './Movie'
 import logo from './logo.svg';
 import './App.css';
 
-const movies = [
-  {
-    id: 1,
-    title: 'Star Wars'
-  },
-  {
-    id: 2,
-    title: 'Infinity Wars'
-  },
-  {
-    id: 3,
-    title: 'Spider Man'
-  }
-];
-
 class App extends Component {
+  
+  state = {
+    movies: []
+  }
+  
+  async componentDidMount() {
+    try {
+      const res = await fetch("https://api.themoviedb.org/3/discover/movie?api_key=a8cdfa69af55f80964d61a4a96025998&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1");
+      
+      const movies = await res.json();
+      console.log(movies)
+      this.setState({
+        movies: movies.results
+      })
+      
+    }catch(e) {
+      console.log(e);
+    }
+  }
+  
   render() {
+    console.log(this.state.movies)
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </header>
-        {movies.map(movie => <Movie key={movie.id} movie={movie} />)}
+        {this.state.movies.map(movie => <Movie key={movie.id} movie={movie} />)}
       </div>
     );
   }
--- src/Movie.js
+++ src/Movie.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Movie extends Component {
+  static propTypes = {
+    movie: PropTypes.shape({
+      title: PropTypes.string.isRequired
+    }),
+    desc: PropTypes.string
+  }
+
   render() {
     return(
       <div>
-        {this.props.movie.title}
+        <h3>{this.props.movie.title}</h3>
       </div>
     );
   }

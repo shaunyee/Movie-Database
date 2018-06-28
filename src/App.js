6702bead77feb6a1b2bb7d5b4c18f03e8c2d5c46
Shaun Yee <yeezy@Shauns-MacBook-Pro.local>
2018-06-27 20:54:02 -0700

    Creates MovieDetail component, component hits api to get details about the movie

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {
 } from 'react-router-dom';
 
 import MoviesList from './MoviesList';
+import MovieDetail from './MovieDetail';
 import logo from './logo.svg';
 import './App.css';
 
@@ -20,14 +21,10 @@ const App = () => (
       </header>
       <Switch>
         <Route exact path="/" component={MoviesList} />
-        <Route path="/:id" component={Test} />
+        <Route path="/:id" component={MovieDetail} />
       </Switch>
     </div>
   </Router>
 );
 
 export default App;
-
-const Test = ({ match }) => (
-  <h1>{match.params.id}</h1>
-);
--- src/MoviesList.js
+++ src/MoviesList.js
@@ -13,10 +13,9 @@ class MoviesList extends Component {
       const res = await fetch(`https://api.themoviedb.org/3/discover/movie?${process.env.REACT_APP_MOVIE_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`);
 
       const movies = await res.json();
-      console.log(movies)
       this.setState({
         movies: movies.results
-      })
+      });
 
     }catch(e) {
       console.log(e);
